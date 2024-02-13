571bd9b2e38a078f0ee4b0beefcb3c64e57b4b93
AlvaroHJ21 <alvarohuaysara@gmail.com>
2024-02-12 20:13:49 -0500

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -24,23 +24,25 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-/**
- * Empresa
- */
-Route::get("/empresas", [EmpresaController::class, "index"])->name("empresas.index");
-Route::get("/empresas/create", [EmpresaController::class, "create"])->name("empresas.create");
-Route::get("/empresas/{empresa}/edit", [EmpresaController::class, "edit"])->name("empresas.edit");
-Route::post("/empresas", [EmpresaController::class, "store"])->name("empresas.store");
-Route::put("/empresas/{empresa}", [EmpresaController::class, "update"])->name("empresas.update");
-Route::put("/empresas/{empresa}/toggle-mode", [EmpresaController::class, "toggleMode"])->name("empresas.toggle_mode");
-Route::delete("/empresas/{empresa}", [EmpresaController::class, "destroy"])->name("empresas.destroy");
+Route::middleware("auth")->group(function () {
+    /**
+     * Empresa
+     */
+    Route::get("/empresas", [EmpresaController::class, "index"])->name("empresas.index");
+    Route::get("/empresas/create", [EmpresaController::class, "create"])->name("empresas.create");
+    Route::get("/empresas/{empresa}/edit", [EmpresaController::class, "edit"])->name("empresas.edit");
+    Route::post("/empresas", [EmpresaController::class, "store"])->name("empresas.store");
+    Route::put("/empresas/{empresa}", [EmpresaController::class, "update"])->name("empresas.update");
+    Route::put("/empresas/{empresa}/toggle-mode", [EmpresaController::class, "toggleMode"])->name("empresas.toggle_mode");
+    Route::delete("/empresas/{empresa}", [EmpresaController::class, "destroy"])->name("empresas.destroy");
 
-/**
- * Entidades
- */
-Route::get("/entidades", [EntidadController::class, "index"])->name("entidades.index");
-Route::get("/entidades/create", [EntidadController::class, "create"])->name("entidades.create");
-Route::get("/entidades/{entidad}/edit", [EntidadController::class, "edit"])->name("entidades.edit");
-Route::post("/entidades", [EntidadController::class, "store"])->name("entidades.store");
-Route::put("/entidades/{entidad}", [EntidadController::class, "update"])->name("entidades.update");
-Route::delete("/entidades/{entidad}", [EntidadController::class, "destroy"])->name("entidades.destroy");
+    /**
+     * Entidades
+     */
+    Route::get("/entidades", [EntidadController::class, "index"])->name("entidades.index");
+    Route::get("/entidades/create", [EntidadController::class, "create"])->name("entidades.create");
+    Route::get("/entidades/{entidad}/edit", [EntidadController::class, "edit"])->name("entidades.edit");
+    Route::post("/entidades", [EntidadController::class, "store"])->name("entidades.store");
+    Route::put("/entidades/{entidad}", [EntidadController::class, "update"])->name("entidades.update");
+    Route::delete("/entidades/{entidad}", [EntidadController::class, "destroy"])->name("entidades.destroy");
+});
